a8d61e8e6a4d6f4341ef9b198346a2f2d64fe611
= <andre.silva@hubdoctor.com.br>
2022-03-29 15:59:42 -0300

    V0

---
+++ ProjApiRest29-03-2022/Data/ProjApiRest29_03_2022Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjApiRest29_03_2022.Model;
+
+namespace ProjApiRest29_03_2022.Data
+{
+    public class ProjApiRest29_03_2022Context : DbContext
+    {
+        public ProjApiRest29_03_2022Context (DbContextOptions<ProjApiRest29_03_2022Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjApiRest29_03_2022.Model.Client> Client { get; set; }
+    }
+}
---
+++ ProjApiRest29-03-2022/Model/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjApiRest29_03_2022.Model
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+    }
+}
